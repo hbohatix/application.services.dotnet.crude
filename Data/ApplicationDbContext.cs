4cb7a09b6be6bef5b2fbcaac36f3ed7a6f5a16a4
Herman Bohatiuk <58301840+hbohatix@users.noreply.github.com>
2024-02-16 18:49:36 +0100

    Initial commit from local repository

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using hbohatix76840.Data.TodoData;
+
+namespace hbohatix76840.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = default!;
+    }
+}
